42cd0d3b9d0f255bcf9bc3e9779650dcd8b7f7e4
Cuauhtemoc Magana <magamen@Cuauhtemocs-MacBook-Air.local>
2022-07-19 08:00:34 -0700

    Added CustomerController to test Swagger Documentation.

--- src/main/java/mendoza/code/test/CustomerManagerApplication.java
+++ src/main/java/mendoza/code/test/CustomerManagerApplication.java
@@ -1,4 +1,10 @@
-package job.mendoza.hiring.code.evaluation.customermanager;
+/*
+ * Copyright (c) 2022. This source code is owned to Cuauhtemoc Magana.
+ *
+ * The code is under the Apache Open Source without limits or restrictions to use.
+ */
+
+package mendoza.code.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/mendoza/code/test/config/SwaggerConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022. This source code is owned to Cuauhtemoc Magana.
+ *
+ * The code is under the Apache Open Source without limits or restrictions to use.
+ */
+
+package mendoza.code.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static mendoza.code.test.constants.ApplicationConstants.*;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(SWAGGER_PACKAGE_NAME))
+                .paths(regex(SWAGGER_REGEX_MANAGER))
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title(SWAGGER_TITLE)
+                .description(SWAGGER_DESCRIPTION)
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Cuauhtemoc M Mendoza", "", "temcmm99@hotmail.com"))
+                .build();
+    }
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
